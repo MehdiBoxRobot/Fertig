57dfefd4f7f373badf0102218ff1d4106927600a
MehdiBoxRobot <esmailimehdi244@yahoo.com>
2025-07-11 00:24:54 +0200

    config.py

--- config.py
+++ config.py
@@ -5,4 +5,7 @@ load_dotenv()
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("TOKEN")  # اینجا مهمه! اسمش 'TOKEN' هست نه 'BOT_TOKEN'
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS").split(",")))
+MONGO_URL = os.getenv("MONGO_URL")
+MONGO_DB = os.getenv("MONGO_DB")

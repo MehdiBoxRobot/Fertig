82c1db4d3afea06d924e404f305072f8080d99ff
MehdiBoxRobot <esmailimehdi244@yahoo.com>
2025-07-10 21:46:08 +0200

    config.py

--- config.py
+++ config.py
@@ -9,4 +9,10 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB = os.getenv("MONGO_DB", "boxoffice")
 
-ADMIN_IDS = [7872708405, 6867380442]
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))  # مثال: "12345,67890"
+REQUIRED_CHANNELS = [
+    "BoxOffice_Animation",
+    "BoxOfficeMoviiie",
+    "BoxOffice_Irani",
+    "BoxOfficeGoftegu"
+]

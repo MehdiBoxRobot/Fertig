335f3ac1f631b949ade49087ba2c39544d4dce49
MehdiBoxRobot <esmailimehdi244@yahoo.com>
2025-07-10 23:46:25 +0200

    database.py

--- database.py
+++ database.py
@@ -5,11 +5,10 @@ client = MongoClient(MONGO_URI)
 db = client[MONGO_DB]
 
 files_col = db["files"]
-stats_col = db["stats"]  # برای آمار ویو، دانلود، اشتراک
+stats_col = db["stats"]
 
 
 async def save_file(film_id, file_id, quality, caption):
-    """ذخیره فایل در دیتابیس"""
     existing = files_col.find_one({"film_id": film_id, "file_id": file_id})
     if existing:
         return False
@@ -19,7 +18,6 @@ async def save_file(film_id, file_id, quality, caption):
         "quality": quality,
         "caption": caption
     })
-    # آمار اولیه
     stats_col.update_one(
         {"film_id": film_id},
         {"$setOnInsert": {"views": 0, "downloads": 0, "shares": 0}},
@@ -27,14 +25,16 @@ async def save_file(film_id, file_id, quality, caption):
     )
     return True
 
+
 async def get_files_by_film_id(film_id):
     return list(files_col.find({"film_id": film_id}))
 
+
 async def increment_stat(film_id, stat_type):
-    """stat_type can be 'views', 'downloads', or 'shares'"""
-    if stat_type not in ['views', 'downloads', 'shares']:
+    if stat_type not in ["views", "downloads", "shares"]:
         return
     stats_col.update_one({"film_id": film_id}, {"$inc": {stat_type: 1}})
+
 
 async def get_stats(film_id):
     stats = stats_col.find_one({"film_id": film_id})

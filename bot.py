17013d4eae40dbc8ee1ce89a48a75bf97d860061
MehdiBoxRobot <esmailimehdi244@yahoo.com>
2025-07-11 00:07:36 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,15 @@
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
+# ربات در حالت bot اجرا بشه، نه user
 app = Client(
-    "BoxOfficeBot",
+    "BoxOfficeUploaderBot",  # این اسم باید ثابت بمونه
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN   # حتما اینجا مقدار توکن ربات رو بدید
+    bot_token=BOT_TOKEN
 )
 
-@app.on_message()
-async def handler(client, message):
-    await message.reply("ربات آماده است.")
-
+# اینجا ربات رو استارت کن
 if __name__ == "__main__":
+    print("Starting bot...")
     app.run()

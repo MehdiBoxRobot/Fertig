7edf551c62065414a2e96bb3968c8f048f93efcf
MehdiBoxRobot <esmailimehdi244@yahoo.com>
2025-07-11 00:23:00 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -2,15 +2,15 @@ from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
 app = Client(
-    name="bot",  # فقط "bot" بذار برای جلوگیری از .session اضافی
+    "BoxOfficeBot",  # اسم پروژه. مهم نیست چی باشه ولی باید ثابت بمونه
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN
+    bot_token=BOT_TOKEN  # این خط باعث میشه Pyrogram وارد حالت Bot بشه
 )
 
 @app.on_message()
-async def hello_handler(client, message):
-    await message.reply("✅ Bot is working!")
+async def start_handler(client, message):
+    await message.reply("✅ ربات آنلاین و سالمه!")
 
 if __name__ == "__main__":
     print("🔥 Bot is starting...")

33294b86c7ad32c6665c2807baf08ea66595eb9c
MehdiBoxRobot <esmailimehdi244@yahoo.com>
2025-07-11 00:07:03 +0200

    config.py

--- config.py
+++ config.py
@@ -6,14 +6,3 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
-
-MONGO_URI = os.getenv("MONGO_URI")
-MONGO_DB = os.getenv("MONGO_DB")
-
-CHANNELS = [
-    "BoxOffice_Animation",
-    "BoxOfficeMoviiie",
-    "BoxOffice_Irani",
-    "BoxOfficeGoftegu"
-]

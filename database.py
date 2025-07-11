0cfa5cdb1fe293f792c5edcf3e1ac6a6a12a84e5
MehdiBoxRobot <esmailimehdi244@yahoo.com>
2025-07-11 15:56:56 +0200

    database.py

--- database.py
+++ database.py
@@ -1,43 +1,38 @@
-from pymongo import MongoClient
-from config import MONGO_URI, MONGO_DB
+import pymongo
+import random
+import string
 
-client = MongoClient(MONGO_URI)
-db = client[MONGO_DB]
+MONGO_URI = "mongodb+srv://your_user:your_password@cluster.mongodb.net/boxoffice?retryWrites=true&w=majority"
 
-files_col = db["files"]
-stats_col = db["stats"]
+class MongoDBClient:
+    def __init__(self):
+        self.client = pymongo.MongoClient(MONGO_URI)
+        self.db = self.client["boxoffice_db"]
+        self.films = self.db["films"]
 
+    def generate_film_id(self, title):
+        # ساخت شناسه یکتا برای فیلم
+        suffix = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
+        base = ''.join(e for e in title if e.isalnum()).lower()
+        return f"{base}_{suffix}"
 
-async def save_file(film_id, file_id, quality, caption):
-    existing = files_col.find_one({"film_id": film_id, "file_id": file_id})
-    if existing:
-        return False
-    files_col.insert_one({
-        "film_id": film_id,
-        "file_id": file_id,
-        "quality": quality,
-        "caption": caption
-    })
-    stats_col.update_one(
-        {"film_id": film_id},
-        {"$setOnInsert": {"views": 0, "downloads": 0, "shares": 0}},
-        upsert=True
-    )
-    return True
+    def add_file(self, film_id, file_id, title, quality, caption):
+        # اضافه کردن فایل به فیلم (مجموعه فایل‌ها)
+        film = self.films.find_one({"film_id": film_id})
+        if film:
+            self.films.update_one(
+                {"film_id": film_id},
+                {"$push": {"files": {"file_id": file_id, "quality": quality, "caption": caption}}}
+            )
+        else:
+            self.films.insert_one({
+                "film_id": film_id,
+                "title": title,
+                "files": [{"file_id": file_id, "quality": quality, "caption": caption}]
+            })
 
-
-async def get_files_by_film_id(film_id):
-    return list(files_col.find({"film_id": film_id}))
-
-
-async def increment_stat(film_id, stat_type):
-    if stat_type not in ["views", "downloads", "shares"]:
-        return
-    stats_col.update_one({"film_id": film_id}, {"$inc": {stat_type: 1}})
-
-
-async def get_stats(film_id):
-    stats = stats_col.find_one({"film_id": film_id})
-    if not stats:
-        return {"views": 0, "downloads": 0, "shares": 0}
-    return {k: stats.get(k, 0) for k in ["views", "downloads", "shares"]}
+    def get_files(self, film_id):
+        film = self.films.find_one({"film_id": film_id})
+        if film:
+            return film.get("files", [])
+        return []
